f02ba7fb080ec8b23374d018519a4f68d0c636c6
imran khan <imran.ayub.khan@accenture.com>
2023-12-14 17:14:45 +0530

    Everythig is fixed now except Minify issue

--- src/App.js
+++ src/App.js
@@ -4,50 +4,43 @@ import './App.css';
 
 function App() {
 
-  let [input, setInput] = useState("");
+  const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
 
   const handlerInput = (event) => {
+    setInput(event.target.value);
+  }
 
-    
-    
-    setInput(event.target.value)
 
-    // setInput(event.target.value.replace(/<---BEGIN---[^]*?---BEGIN--->/g, ''));
-
-    
+  function extractJsonArray(inputCode) {
+    const regex = /\[([\s\S]*?)\]/;
+    const match = regex.exec(inputCode);
+  
+    if (match && match[1]) {
+      return match[0]; // Entire matched array including brackets
+      // Alternatively, return match[1]; // Only the content of the array without brackets
+    }
+  
+    return null;
   }
 
   const handlerFormat = () => {
-
-    function extractJsonArray(inputCode) {
-      const regex = /\[([\s\S]*?)\]/;
-      const match = regex.exec(inputCode);
     
-      if (match && match[1]) {
-        return match[0]; // Entire matched array including brackets
-        // Alternatively, return match[1]; // Only the content of the array without brackets
-      }
-    
-      return null;
-    }
-    
-   
     const inputCode = input;
-    
     const extractedJsonArray = extractJsonArray(inputCode);
-    
     if (extractedJsonArray !== null) {
+      
+      const finalOutputWithoutScript = JSON.stringify(JSON.parse(extractedJsonArray), null, 2);
+      const finalOutputWithScript = `<script type='application/ld+json'> ${finalOutputWithoutScript} </script>`
 
-      const finalOutput = JSON.stringify(JSON.parse(extractedJsonArray), null, 2);
-
-      const index = JSON.parse(finalOutput).findIndex((el) => {
-        return el["@type"] === "MedicalAudience";
-      });
+      // const index = JSON.parse(finalOutput).findIndex((el) => {
+      //   return el["@type"] === "MedicalAudience";
+      // });
 
       
 
-      setOutput(finalOutput);
+      setOutput(finalOutputWithScript);
+      // setOutput(finalOutput);
       // setOutput(JSON.stringify(JSON.parse(extractedJsonArray), null, 2));
       // setOutput(JSON.parse(extractedJsonArray));
       // console.log(JSON.stringify(JSON.parse(extractedJsonArray), null, 2))
@@ -70,6 +63,32 @@ function App() {
     }
 
   }
+
+
+  const handlerCommentRemove = () => {
+    const inputCode = input;
+    const extractedJsonArray = extractJsonArray(inputCode);
+    if (extractedJsonArray !== null) {
+      
+      const finalOutputWithoutScript = extractedJsonArray ;
+      const finalOutputWithScript = `<script type='application/ld+json'> ${finalOutputWithoutScript} </script>`
+
+      // const index = JSON.parse(finalOutput).findIndex((el) => {
+      //   return el["@type"] === "MedicalAudience";
+      // });
+
+      
+
+      setOutput(finalOutputWithScript);
+      // setOutput(finalOutput);
+      // setOutput(JSON.stringify(JSON.parse(extractedJsonArray), null, 2));
+      // setOutput(JSON.parse(extractedJsonArray));
+      // console.log(JSON.stringify(JSON.parse(extractedJsonArray), null, 2))
+
+    } else {
+      setOutput("No JSON array found in the code.");
+    }
+  }
   
   
   
@@ -81,11 +100,12 @@ function App() {
       <textarea className='large-area large-area--input' value={input} onChange={handlerInput} placeholder='Input' />
 
       <div className="controls">
+        <button onClick={handlerCommentRemove} type='button' className='btn-comment-remove'> Remove Comment </button>
         <button onClick={handlerFormat} type='button' className='btn-format'> Format </button>
         <button onClick={handlerMinify} type='button' className='btn-minify'> Minify </button>
       </div>
 
-      <textarea className='large-area large-area--output' readOnly value={output} placeholder='Output' />
+      <textarea className='large-area large-area--output' value={output} placeholder='Output' readOnly />
 
     </div>
   );
